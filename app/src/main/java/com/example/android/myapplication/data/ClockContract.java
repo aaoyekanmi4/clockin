d87e3876ca45c06a68798854339fbc90a208ae22
aaoyekanmi4 <aaoyekanmi4@gmail.com>
2018-03-10 17:34:18 -0600

    Added Uri constants to ClockContract.java

--- app/src/main/java/com/example/android/myapplication/data/ClockContract.java
+++ app/src/main/java/com/example/android/myapplication/data/ClockContract.java
@@ -1,5 +1,6 @@
 package com.example.android.myapplication.data;
 
+import android.net.Uri;
 import android.provider.BaseColumns;
 /**
  * Created by Arit on 11/30/2017.
@@ -8,7 +9,17 @@ import android.provider.BaseColumns;
 public final class ClockContract {
     private ClockContract() {}
 
+    public static final String CONTENT_AUTHORITY = "com.example.android.myapplication";
+
+
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+
+    public static final String PATH_CLOCK = "clock";
+
     public static final class ClockEntry implements BaseColumns {
+
+        /** The content URI to access the clock data in the provider */
+        public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_CLOCK);
         /**
          * Name of database table for pets
          */
@@ -16,8 +27,9 @@ public final class ClockContract {
 
         /**
          * Unique ID number for the clock in row (only for use in the database table).
-         * <p>
+         *
          * Type: INTEGER
+         *
          */
         public final static String _ID = BaseColumns._ID;
 
